1aecdad3c6aeadf443bf5eb64024bce25bf60dd3
TekCastPork <buckblaster6x7@gmail.com>
2018-03-28 12:54:33 -0600

    Adding a Whats New splash screen for when you update the program
    May turn this into a welcome splash screen at some point, but for now it will be for changes when updates happen.

---
+++ BattleBabs Client/BattleBabs Client/WhatsNewScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleBabs_Client
+{
+    public partial class WhatsNewScreen : Form
+    {
+        public WhatsNewScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
