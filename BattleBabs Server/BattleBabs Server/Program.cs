d239e97547690a42b2a098a58798b51b8175af6d
TekCastPork <buckblaster6x7@gmail.com>
2018-05-12 23:31:29 -0600

    Began progress on version 3
    Structure-Rewrite was going to be version 3, but the commit before states why that branch is no longer being used.
    
    To help remind myself of what is completed this branch has a Todo list file.

---
+++ BattleBabs Server/BattleBabs Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleBabs_Server
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Appdata location: {0}", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadExit += isExiting;
+            Application.Run(new Main());
+        }
+
+        static void isExiting(object sender, System.EventArgs e)
+        {
+            Console.WriteLine("Main thread is exiting!");
+        }
+    }
+}
---
+++ BattleBabs Server/TODO LIST.md
@@ -0,0 +1,15 @@
+* Add GameData class
+	* assist class for items such as team structure
+	* Team structure -> Simplified for current time being
+		* string Name
+		* int score
+	* Use list to hold structures - more powerful and useful
+* Add networking class
+* Get networking class to work correctly with current scoreboard program
+***Achieve above before working on below***
+* Create match list window
+	* Use a scrollable list instead of big window -> easier to view and smaller window required
+* Create session load/save
+* Create Log file generating class
+
+**Ensure any savable data gets stored to %appdata% and not the working directory**
